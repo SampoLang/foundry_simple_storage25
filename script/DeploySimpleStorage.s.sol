cc0eb0cf8cf96f360a2d26307a822d01faa77c54
SampoLang <sampo.lang@gmail.com>
2025-04-05 04:49:22 +0000

    lesson 6 complete

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -1,11 +1,16 @@
 // SPDX-Licence-identifier: MIT
 
 pragma solidity ^0.8.19;
+
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
+    //run gets called when we run the script
     function run() external returns (SimpleStorage) {
+        //startBroadcast is used to start the transaction
+        //vm is a global variable that is used to access the forge-std library
+        //everything after startbroadcast is sent to the RPC
         vm.startBroadcast();
         SimpleStorage simpleStorage = new SimpleStorage();
         vm.stopBroadcast();
